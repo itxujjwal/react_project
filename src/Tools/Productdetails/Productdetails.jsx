fc1707ba6c6a4bdbcbda4ee4fa9519a621126749
itxujjwal <ranaujjwal938@gmail.com>
2024-08-20 18:10:20 +0530

    Removed React Image Magnify component

--- src/Tools/Productdetails/Productdetails.jsx
+++ src/Tools/Productdetails/Productdetails.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import ReactImageMagnify from "react-image-magnify"
 import { useParams } from "react-router-dom";
 import { Button } from "@mui/material";
 import { addToCart } from "../../Store/slices/Cartslice";
 import { useDispatch } from "react-redux";
 import toast, { Toaster } from "react-hot-toast";
+
 const Productdetails = () => {
   const _useParams = useParams();
   let { id } = useParams();
@@ -16,7 +16,7 @@ const Productdetails = () => {
     image: "",
     description: "",
   });
-  // alert(id)
+
   useEffect(() => {
     window.scrollTo(0, 5);
     axios
@@ -29,34 +29,18 @@ const Productdetails = () => {
       });
   }, [id]);
 
-  const addToCartData=(item)=>{
+  const addToCartData = (item) => {
     dispatch(addToCart(item))
     toast.success("Item added successfully")
   }
+
   return (
     <>
       <div className="container">
-      <Toaster/>
+        <Toaster/>
         <div className="row">
           <div className="col-md-4 p-3">
-            <div style={{ width: "500px" }}>
-              <ReactImageMagnify
-                {...{
-                  smallImage: {
-                    alt: "Wristwatch by Ted Baker London",
-
-                    src: product.image,
-                    width: 345,
-                    height: 412,
-                  },
-                  largeImage: {
-                    src: product.image,
-                    width: 800,
-                    height: 1100,
-                  },
-                }}
-              />
-            </div>
+            <img src={product.image} alt={product.title} style={{ width: "500px" }} />
           </div>
           <div className="col-md-6 p-5">
             <div className="card-body">
@@ -65,7 +49,7 @@ const Productdetails = () => {
               <p class="card-text">₹{product.price}</p>
               <p class="card-text">{product.description}</p>
               <br />
-              <Button variant="contained" size="medium" onClick={()=>addToCartData(product)}>Add to cart</Button>
+              <Button variant="contained" size="medium" onClick={() => addToCartData(product)}>Add to cart</Button>
             </div>
           </div>
         </div>
